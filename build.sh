fbb3f22bdfc5a689e98a0c16e56c3a89e6e96bfd
Monstrofil <shalal545@gmail.com>
2016-09-25 11:50:43 +0300

    'Warnings'

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 python prebuild.py
-/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsEarsConfigUbuntu.xml -omit-trace-statements -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
+/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsEarsConfigUbuntu.xml -omit-trace-statements -warnings=false -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
 
 mkdir ml-ears
 python file_divide.py config/__background.xml bin/ml-ears-world-of-warships/ root_block
