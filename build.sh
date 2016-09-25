51057c7cd053d09e985a6b1d9ac3825ad9546f52
Monstrofil <shalal545@gmail.com>
2016-09-25 11:39:53 +0300

    'BuildScript'

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 python prebuild.py
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsEarsConfigUbuntu.xml -omit-trace-statements -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
 
