19e5835de25a3c1974a2f9ef6ab9ce853becfe4c
Monstrofil <shalal545@gmail.com>
2016-09-25 12:13:46 +0300

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 PROJECT="ml-ears-world-of-warships"
-
 set -eo pipefail
-
 python prebuild.py
 
-
-mkdir -R gui/flash/
-
+mkdir -p gui/flash/
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=$PROJECT.xml -omit-trace-statements -warnings=false -debug=false -incremental=true -swf-version=12 -o gui/flash/$PROJECT.swf
-
 python file_divide.py config/__background.xml gui/flash/$PROJECT/ root_block
