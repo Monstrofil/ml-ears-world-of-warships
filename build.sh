481894f1c622c6a7c9661a6bcb2c8465005b30ee
Monstrofil <shalal545@gmail.com>
2016-09-25 12:11:26 +0300

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+PROJECT="ml-ears-world-of-warships"
+
 set -eo pipefail
 
 python prebuild.py
-/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsEarsConfigUbuntu.xml -omit-trace-statements -warnings=false -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
 
-mkdir ml-ears
-python file_divide.py config/__background.xml bin/ml-ears-world-of-warships/ root_block
+
+mkdir -R gui/flash/
+
+/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=$PROJECT.xml -omit-trace-statements -warnings=false -debug=false -incremental=true -swf-version=12 -o gui/flash/$PROJECT.swf
+
+python file_divide.py config/__background.xml gui/flash/$PROJECT/ root_block
